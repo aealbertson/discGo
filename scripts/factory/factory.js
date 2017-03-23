1b89ec5a163f629d7ac4292ba6437fbac237926e
Ashton Albertson <ashtonalbertson@gmail.com>
2017-03-22 20:43:46 -0400

    added variable to ctrl

--- scripts/factory/factory.js
+++ scripts/factory/factory.js
@@ -13,11 +13,14 @@ app.factory('discGoFactory', function($http) {
         url: 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + searchCriteria + '&api_key=2f32cf4dbf47aa1b214b2cb2d18f9e24&format=json',
         }).then(function successCallback(response) {
           console.log(response);
-          console.log(response.data.artist.name);
+          // console.log(response.data.artist.name);
+          var artistName = response.data.artist.name
+          console.log(artistName);
           searchResult = response;
     });
   return promise;
 }
+  $('section').append(artistName);
 });
 
 // app.controller('test', function($scope, discGoFactory){

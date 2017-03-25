c576982fc7109b2bb694e3904b9a96c5dd9fdb2d
Ramiz Ali <zimarali@gmail.com>
2017-03-25 09:32:25 -0400

    created new factory.js

--- scripts/factory/factory.js
+++ scripts/factory/factory.js
@@ -1,10 +1,10 @@
-var app = angular.module('discGo',[]);
+var app = angular.module('discGo', []);
 
 app.factory('discGoFactory', function($http) {
 
   var finalData = {};
 
-  return{
+  return {
     finalData: finalData,
     searchResults : searchResults
   };
@@ -12,22 +12,23 @@ app.factory('discGoFactory', function($http) {
   function searchResults(searchCriteria) {
     var promise = $http({
         method: 'GET',
-        url: 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + searchCriteria + '&api_key=2f32cf4dbf47aa1b214b2cb2d18f9e24&format=json',
+        url: 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + searchCriteria + '&api_key=2f32cf4dbf47aa1b214b2cb2d18f9e24&format=json'
         }).then(function successCallback(response) {
           console.log(response);
           finalData.artistName = response.data.artist.name;
           finalData.artistBio = response.data.artist.bio.content;
-          finalData.artistImage = response.data.artist.image[3]; // image is stored in an object. Need to be able to pull out the url.
-          console.log(finalData.artistName);
-          console.log(finalData.artistBio);
-          console.log(finalData.artistImage); // will need to go in an image tag <img src = object.text />
+        }, function(error) {
+          console.log(error);
+
+          // finalData.artistImage = response.data.artist.image[3]; // image is stored in an object. Need to be able to pull out the url.
+          // console.log(finalData.artistName);
+          // console.log(finalData.artistBio);
+          // console.log(finalData.artistImage); // will need to go in an image tag <img src = object.text />
     });
   return promise;
   }
   function returnResults(){
     return finalData;
-    console.log(finalData);
-  }
   }
 
 });
---
+++ scripts/factory/factorynew.js
@@ -0,0 +1,31 @@
+var app = angular.module('discGo');
+
+app.factory('discGoFactory', function($http) {
+
+  var finalData = {};
+
+  return {
+    finalData: finalData,
+    searchResults: searchResults
+  };
+
+  function searchResults(searchCriteria) {
+      var promise = $http({
+        method: 'GET',
+        url: 'http://ws.audioscrobbler.com/2.0/?method=artist.getinfo&artist=' + searchCriteria + '&api_key=2f32cf4dbf47aa1b214b2cb2d18f9e24&format=json'
+      }).then(function successfulCallback(response) {
+        console.log(response);
+        finalData.artistName = response.data.artist.name;
+        finalData.artistBio = response.data.artist.bio.content;
+      }, function(error) {
+        console.log(error);
+      });
+    return promise;
+  }
+
+  function returnResults() {
+    return finalData;
+  }
+
+
+});

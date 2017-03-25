aae110070a1110864dc8202b4827da74424f4e35
Ramiz Ali <zimarali@gmail.com>
2017-03-25 11:33:15 -0400

    working image/bio html

--- scripts/controllers/print-ctrl.js
+++ scripts/controllers/print-ctrl.js
@@ -4,3 +4,10 @@ app.controller('printCtrl', function($scope, discGoFactory){
   $scope.artistInfo = discGoFactory.returnResults();
   console.log($scope.artistInfo);
 });
+
+app.filter('unsafe', function($sce){
+  return function(val) {
+    return $sce.trustAsHtml(val);
+  };
+
+});

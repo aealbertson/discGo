63593231cb2b16626af873e7af1492d43d76768a
Ramiz Ali <zimarali@gmail.com>
2017-03-27 21:01:50 -0400

    album data, layout changes

--- scripts/controllers/print-ctrl.js
+++ scripts/controllers/print-ctrl.js
@@ -4,15 +4,16 @@ app.controller('printCtrl', function($scope, discGoFactory){
   $scope.albums = discGoFactory.returnAlbumData();
   $scope.artistInfo = discGoFactory.returnResults();
   console.log($scope.artistInfo);
-
-  $scope.topFive = [];
-  //var albums = discGoFactory.searchAlbum($scope.artistInfo.artistName).data.topAlbums.album;
-  //discGoFactory.searchAlbum($scope.artistInfo.artistName);
-  // $scope.albums = discGoFactory.returnAlbumData();
   console.log($scope.albums);
 
-  for (var i=0; i<5; i++){
-  }
+  // $scope.topFive = [];
+  // //var albums = discGoFactory.searchAlbum($scope.artistInfo.artistName).data.topAlbums.album;
+  // //discGoFactory.searchAlbum($scope.artistInfo.artistName);
+  // // $scope.albums = discGoFactory.returnAlbumData();
+  //
+  //
+  // for (var i=0; i<5; i++){
+  // }
 });
 
 app.filter('unsafe', function($sce){
--- scripts/factory/factory.js
+++ scripts/factory/factory.js
@@ -17,10 +17,19 @@ app.factory('discGoFactory', function($http) {
   function searchAlbum(searchCriteria) {
       $http({
         method: 'GET',
-        url: 'http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=' + searchCriteria + '&api_key=2f32cf4dbf47aa1b214b2cb2d18f9e24&format=json'
+        url: 'http://ws.audioscrobbler.com/2.0/?method=artist.gettopalbums&artist=' + searchCriteria + '&api_key=2f32cf4dbf47aa1b214b2cb2d18f9e24&limit=5&format=json'
       }).then(function successfulCallback(response) {
-        albumData = response.data.topalbums.album[0];
         console.log(albumData);
+        albumData.albumName1 = response.data.topalbums.album["0"].name;
+        albumData.albumImage1 = response.data.topalbums.album["0"].image[1]["#text"];
+        albumData.albumName2 = response.data.topalbums.album["1"].name;
+        albumData.albumImage2 = response.data.topalbums.album["1"].image[1]["#text"];
+        albumData.albumName3 = response.data.topalbums.album["2"].name;
+        albumData.albumImage3 = response.data.topalbums.album["2"].image[1]["#text"];
+        albumData.albumName4 = response.data.topalbums.album["3"].name;
+        albumData.albumImage4 = response.data.topalbums.album["3"].image[1]["#text"];
+        albumData.albumName5 = response.data.topalbums.album["4"].name;
+        albumData.albumImage5 = response.data.topalbums.album["4"].image[1]["#text"];
       }, function(error) {
         console.log(error);
       });
